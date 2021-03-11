eaabd8249bfc6a53ffb001903059ee95660521c4
Pavel Novikov <pavel_novikov1@epam.com>
2021-03-11 22:30:43 +0300

    add .gitignore, add h2 db, clean up poms

--- entities/src/main/java/org/example/entities/Order.java
+++ entities/src/main/java/org/example/entities/Order.java
@@ -1,13 +1,17 @@
 package org.example.entities;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
+@Entity(name = "orders")
 @Accessors(chain = true)
 public class Order {
 
+  @Id
   private Integer id;
   private String name;
 }
